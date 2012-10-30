7415feb046f1f3455da79988cb7b930d3cc8fb1e
Matt Smith <tangohead@gmail.com>
2012-10-30 00:44:33 +0000

    Work on preferential attachment
    Using Barabási–Albert model

--- NetworkDevelopment/src/NetworkDevelopment/ModelDevBuilder.java
+++ NetworkDevelopment/src/NetworkDevelopment/ModelDevBuilder.java
@@ -8,22 +8,41 @@ import repast.simphony.context.space.graph.NetworkGenerator;
 import repast.simphony.context.space.graph.WattsBetaSmallWorldGenerator;
 import repast.simphony.dataLoader.ContextBuilder;
 import repast.simphony.space.graph.Network;
+import repast.simphony.space.graph.RepastEdge;
+import repast.simphony.util.collections.IndexedIterable;
 
 public class ModelDevBuilder implements ContextBuilder<Object> {
 
 	@Override
 	public Context build(Context<Object> context) {
 		// TODO Auto-generated method stub
-		context.setId("testContext");
-		int nodeCount = 500;
-		for( int i = 0; i < nodeCount ; i ++) {
-			context.add(new Node("n" + i));
+		context.setId("NetworkDevelopment");
+		NetworkBuilder<Node> builder = new NetworkBuilder("TestNet", context, true);
+		Network<Node> net = builder.buildNetwork();
+		context.add(new Node("n0"));
+		context.add(new Node("n1"));
+		context.add(new Node("n2"));
+		context.add(new Node("n3"));
+		
+		IndexedIterable<Object> startNodes = context.getObjects(Node.class);
+		net.addEdge((Node)startNodes.get(0), (Node)startNodes.get(1));
+		net.addEdge((Node)startNodes.get(1), (Node)startNodes.get(2));
+		net.addEdge((Node)startNodes.get(2), (Node)startNodes.get(3));
+		net.addEdge((Node)startNodes.get(3), (Node)startNodes.get(0));
+		
+		//net.addEdge((Node)startNodes.get(1), (Node)startNodes.get(0));
+		
+		int nodeCount = 100;
+		for( int i = 2; i < nodeCount ; i ++) {
+			context.add(new Node("n" + i, context, net));
 		}
 		
-		NetworkGenerator gen = new WattsBetaSmallWorldGenerator(0.2, 2, false);
-		NetworkBuilder builder = new NetworkBuilder("TestNet", context, true);
+		
+		/*NetworkGenerator gen = new WattsBetaSmallWorldGenerator(0.2, 2, false);
+		
 		builder.setGenerator(gen);
-		Network net = builder.buildNetwork();
+		*/
+		
 		return context;
 	}
 	
--- NetworkDevelopment/src/NetworkDevelopment/Node.java
+++ NetworkDevelopment/src/NetworkDevelopment/Node.java
@@ -1,10 +1,15 @@
 package NetworkDevelopment;
 import java.util.ArrayList;
 
+import repast.simphony.context.Context;
+import repast.simphony.space.graph.Network;
+
 
 public class Node {
 	String id;
 	double weight;
+	Context context;
+	Network network;
 	
 	ArrayList<DataAttribute> attributeList;
 	
@@ -12,6 +17,47 @@ public class Node {
 	{
 		this.id = id;
 		this.weight = 1;
+	}
+	
+	Node(String id, Context context, Network<Node> network)
+	{
+		this.id = id;
+		this.weight = 1;
+		this.network = network;
+		this.context = context;
+		
+		Iterable<Node> nodes = network.getNodes();
+		System.out.println("WORKING ON NODE " + id);
+		ArrayList<ArrayList<Node>> newEdges = new ArrayList<ArrayList<Node>>();
+		for(Node current : nodes)
+		{
+			System.out.println("Checking " + id + " against " + current.id);
+			double iDegree = network.getDegree(current);
+			double jSumDegree = 0;
+			for(Node n : network.getNodes())
+			{
+				jSumDegree += network.getDegree(n);
+				//System.out.println("NODE " + n.id + " has degree " +network.getDegree(n) );
+			}
+			
+			double prob = iDegree / jSumDegree;
+			System.out.println("PROB: " + prob + " SUM-J " + jSumDegree + " I " + iDegree);
+			if(Math.random() <= prob)
+			{
+				/*network.addEdge(this, current);*/
+				ArrayList<Node> pair = new ArrayList<Node>();
+				pair.add(this);
+				pair.add(current);
+				newEdges.add(pair);
+				System.out.println("\tAdding edge");
+			}
+		}
+		
+		for(ArrayList<Node> pair : newEdges)
+		{
+			network.addEdge(pair.get(0), pair.get(1));
+		}
+		
 	}
 	
 	Node(String id, double weight)
