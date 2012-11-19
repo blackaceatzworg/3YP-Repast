ff98dad388c093e4f6e42ef9ba067649a8177177
Matt Smith <tangohead@gmail.com>
2012-11-19 19:03:51 +0000

    Started separating analysis out.

---
+++ NetworkDevelopment/src/NetworkDevelopment/SamplerMain.java
@@ -0,0 +1,18 @@
+package NetworkDevelopment;
+
+import edu.uci.ics.jung.graph.Graph;
+
+public class SamplerMain {
+	public static void main(String[] args)
+	{
+		System.out.println("Whattup");
+		//AnalysisTools.repastNetworkToGraphML(context, net, "test.graphml");
+		StanfordParser sp = new StanfordParser("soc-Epinions1.txt");
+		sp.Parse();
+		
+		Graph<String, Long> testGraph;
+		testGraph = sp.toGraph();
+		
+		System.out.println("Nodes: " + testGraph.getVertexCount());
+	}
+}
